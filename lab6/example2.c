ca8e655f07aadace324b89868ecbc85455e7641d
meldilen <123398352+meldilen@users.noreply.github.com>
2024-10-02 20:04:13 +0300

    Update example2.c

--- lab6/example2.c
+++ lab6/example2.c
@@ -32,42 +32,42 @@ int main() {
     
     //part 1
 
-    // signal(SIGINT, sigint_handler);
-    // pid_t pid = fork();
-    // if (pid == 0) {
-    //     alarm(3);
-    //     signal(SIGALRM, alarm_handler);
-    //     while (1) {
-    //         printf("Child hello world!\n");
-    //         sleep(1);
-    //     }
-    // } else if (pid > 0) {
-    //         wait(NULL);
-    //     } else {
-    //         perror("Fork creation error!");
-    //         exit(1);
-    // }
+    signal(SIGINT, sigint_handler);
+    pid_t pid = fork();
+    if (pid == 0) {
+        alarm(3);
+        signal(SIGALRM, alarm_handler);
+        while (1) {
+            printf("Child hello world!\n");
+            sleep(1);
+        }
+    } else if (pid > 0) {
+            wait(NULL);
+        } else {
+            perror("Fork creation error!");
+            exit(1);
+    }
 
     //part 2
 
-    char input[100];
-    signal(SIGUSR1, sigusr1_handler);
-    signal(SIGUSR2, sigusr2_handler);
-    printf("Enter a positive integer: ");
-    fgets(input, 100, stdin);
-    if (input[0] == '-'){
-        raise(SIGUSR1);
-        return 1;
-    }
-    if (input[0] == '\n'){
-        raise(SIGUSR2);
-        return 1;
-    }
-    for (int i = 0; input[i] != '\0'; i++){
-        if (isdigit(input[i])){continue;} else {
-            raise(SIGUSR2);
-            return 1;
-        }
+    // char input[100];
+    // signal(SIGUSR1, sigusr1_handler);
+    // signal(SIGUSR2, sigusr2_handler);
+    // printf("Enter a positive integer: ");
+    // fgets(input, 100, stdin);
+    // if (input[0] == '-'){
+    //     raise(SIGUSR1);
+    //     return 1;
+    // }
+    // if (input[0] == '\n'){
+    //     raise(SIGUSR2);
+    //     return 1;
+    // }
+    // for (int i = 0; input[i] != '\0'; i++){
+    //     if (isdigit(input[i])){continue;} else {
+    //         raise(SIGUSR2);
+    //         return 1;
+    //     }
     }
     return 0;
 }
